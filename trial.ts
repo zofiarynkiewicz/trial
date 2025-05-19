90e0ddc05ebaba524aa3a441c45994f4e1ee1bd4
zofiarynkiewicz <131818687+zofiarynkiewicz@users.noreply.github.com>
2025-05-19 23:57:01 +0200

    Update trial.ts
    cdc

--- trial.ts
+++ trial.ts
@@ -3,5 +3,8 @@
 const apiKey = "ghs_dummydummydummydummydummydummy";
 // test.ts
 const secret = "ghp_abcdABCDabcdABCDabcdABCDabcdABCDabcd"; // 40 chars after `ghp_`
+// test-secret.ts
+const pat = "ghp_1234567890abcdefghijklmnopqrstuvwxyzAB";
+
 
 
